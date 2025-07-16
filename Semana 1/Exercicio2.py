15b982ea3d696105488e2fa5b3c85e2e037f7976
Linapeter <linapark331@gmail.com>
2025-07-16 11:27:37 -0300

    modifying exercicio 2

--- Semana 1/Exercicio2.py
+++ Semana 1/Exercicio2.py
@@ -17,11 +17,17 @@ class students:
         self.grade = grade
 
     def __repr__(self):
-        return "Student {} in grade {} was added.".format(self.name, self.grade)
+        return f"Student {self.name} in grade {self.grade}."
 
+    # Sorting students by grade and name
     def sort_students(self):
         sorted_list = sorted(students.list_students, key=lambda x: (x[0], x[1]))
         return [x[1] for x in sorted_list]
+
+# Function to return students by grade
+def by_grade(grade):
+    in_grade = sorted(students.list_students, key=lambda x: x[0])
+    return [x[1] for x in in_grade if x[0] == grade]
 
 s1 = students("Murphy", 1)
 print(s1)
@@ -29,9 +35,13 @@ s2 = students("Vivian", 2)
 s3 = students("Charlie", 5)
 s4 = students("Alice", 2)
 s5 = students("Carlos", 2)
+s6 = students("Bob", 3)
 s1.sort_students()
 s2.sort_students()
 s3.sort_students()
 s4.sort_students()
 s5.sort_students()
+s6.sort_students()
 print(s1.sort_students())
+print(students.list_students)
+print("Students in grade 2:", by_grade(2))
