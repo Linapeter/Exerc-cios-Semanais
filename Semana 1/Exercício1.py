f8b9f6f6c5cbb9fad2eee7793fa382818d716520
Linapeter <linapark331@gmail.com>
2025-07-21 10:51:31 -0300

    Week2

--- Semana 1/Exercício1.py
+++ Semana 1/Exercício1.py
@@ -11,3 +11,11 @@ for i in range (1,n):
         multiples.append(i)
 
 print("The sum of the multiples is", sum(multiples))
+
+# Correção
+# Aqui, não crio uma lista que consome muita memória
+total = 0
+for i in range(1, 1000):
+    if i%3 == 0 or i%5 == 0:
+        total += i
+print("The sum of the multiples is", total)
