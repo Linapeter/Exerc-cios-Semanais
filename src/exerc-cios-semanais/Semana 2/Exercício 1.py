15fbc771eeac584b09a741b5b1b1094514295b89
Linapeter <linapark331@gmail.com>
2025-07-23 12:00:03 -0300

    setting up poetry

--- src/exerc-cios-semanais/Semana 2/Exercício 1.py
+++ src/exerc-cios-semanais/Semana 2/Exercício 1.py
@@ -1,12 +1,12 @@
 # Exercise 1: Fibonnaci Sequence Sum of even-valued terms
 
 fn1,fn2 = 1, 2
-valued_terms = 0
+even_terms = 0
 limit = 4_000_000
 
 while fn2 < limit:
     if fn2%2 == 0:
-        valued_terms += fn2
+        even_terms += fn2
     fn3 = fn1 + fn2
     fn1 = fn2
     fn2 = fn3
