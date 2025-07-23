7c12b4516cfd80f10e59e32f12961068a765b8fb
Linapeter <linapark331@gmail.com>
2025-07-23 11:24:34 -0300

    correction

--- Semana 2/Exercício 1.py
+++ Semana 2/Exercício 1.py
@@ -1,11 +1,14 @@
-# Exercise 1: Fibonnaci Sequence Sum od even-valued terms
+# Exercise 1: Fibonnaci Sequence Sum of even-valued terms
 
-seq = [1, 2]
-while seq[-1] < 4_000_000:
-    seq.append(seq[-1]+seq[-2])
+fn1,fn2 = 1, 2
+valued_terms = 0
+limit = 4_000_000
 
-a = 0
-for i in seq:
-    if i%2 == 0:
-        a += i
-print(a)
+while fn2 < limit:
+    if fn2%2 == 0:
+        valued_terms += fn2
+    fn3 = fn1 + fn2
+    fn1 = fn2
+    fn2 = fn3
+
+print(valued_terms)
--- Semana 2/Exercício 2.py
+++ Semana 2/Exercício 2.py
@@ -1,7 +1,9 @@
 # Exercicio 2: Sum squares difference
 
-a = (sum(a**2 for a in range(1,101)))
+limit = 101
 
-b = ((sum(i for i in range(1,101)))**2)
+sum_of_squares = (sum(sum_of_squares**2 for sum_of_squares in range(1,limit)))
 
-print(b - a)
+b = ((sum(i for i in range(1,limit)))**2)
+
+print(b - sum_of_squares)
